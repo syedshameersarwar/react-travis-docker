09b0e0a5907e3b7d82f97b816a81c22e0516c6b4
syedshameersarwar <syedshameersarwar@gmail.com>
2020-01-12 22:41:27 +0500

    Removing Jest Error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Travis
+          Deploying with features
         </a>
       </header>
     </div>
